87269f73af1da2d31d8252edd14a596a034c2af5
bingbinglai <imbingbinglai@gmail.com>
2018-08-01 07:57:14 -0400

    refactor again

--- model.py
+++ model.py
@@ -21,13 +21,6 @@ _DROPOUT_RATE = 0.2
 _SHARP_ANGEL_THESHOLD = 0.15
 
 
-'where the driving log are stored'
-_CSV_FILE_BASE = './all_training_data/useful/{}/driving_log.csv'
-
-
-"path to the corresponding image files that's recorded in the csvs"
-_IMAGE_FILE_BASE = './all_training_data/useful/{}/IMG/'
-
 
 def train():
     parent_dirs = [
@@ -50,20 +43,18 @@ def train():
     ]
 
     all_images = []
-    all_measurements = []
+    all_steerings = []
     for the_dir in parent_dirs:
-        _images, _measurements = _get_images_and_measurements(the_dir)
+        _images, _steerings = _get_images_and_steerings(the_dir)
         print('dir: {}, images: {}'.format(the_dir, len(_images)))
         all_images.extend(_images)
-        all_measurements.extend(_measurements)
-
-
+        all_steerings.extend(_steerings)
     print('total images', len(all_images))
-    print('total measurements', len(all_measurements))
+    print('total steerings', len(all_steerings))
     X_train = np.array(all_images)
-    y_train = np.array(all_measurements)
+    y_train = np.array(all_steerings)
 
-
+    # the model
     model = Sequential()
     model.add(Lambda(lambda x: x / 255.0 -0.5, input_shape=(160,320,3)))
     model.add(Cropping2D(cropping=((70,25), (0,0))))
@@ -86,100 +77,82 @@ def train():
     print('Done')
 
 
-def _get_images_and_measurements(parent_dir):
+def _get_images_and_steerings(parent_dir):
     '''
     given a parent directory, parse all the images and the angle
     '''
-    return_images = []
-    return_measurements = []
+    # where the driving log are stored
+    _CSV_FILE_BASE = './all_training_data/useful/{}/driving_log.csv'
+    # path to the corresponding image files that's recorded in the csvs
+    _IMAGE_FILE_BASE = './all_training_data/useful/{}/IMG/'
 
-    for line in _get_csv_lines(parent_dir):
+    def _get_image_path(path):
+        return path.split('/')[-1]
 
+    def _get_csv_lines(parent_dir):
+        lines = []
+        csv_file = _CSV_FILE_BASE.format(parent_dir)
         image_file_base = _IMAGE_FILE_BASE.format(parent_dir)
+        with open(csv_file) as csvfile:
+            reader = csv.reader(csvfile)
+            for line in reader:
+                lines.append(line)
 
-        # center image
-        image = cv2.imread(
-            image_file_base + _get_image_path(line[0])
-        )
+        lines = iter(lines)
+        _ = next(lines)
+        return lines
 
-        if image is None:
-            continue
-        image = _apply_random_brightness(image)
+    def _left_turning(angle):
+        return angle < (-1 * _SHARP_ANGEL_THESHOLD)
 
-        # add the image as it is and also when its flipped version
-        return_images.append(image)
-        return_images.append(cv2.flip(image, 1))
-        measurement = float(line[3])
-        return_measurements.append(measurement)
-        return_measurements.append(measurement * -1.0)
+    def _right_turning(angle):
+        return angle > 1 * _SHARP_ANGEL_THESHOLD
 
+    return_images = []
+    return_steerings = []
+    for line in _get_csv_lines(parent_dir):
+        center, left, right, steering, _, _, _ = line
+        img_path_base = _IMAGE_FILE_BASE.format(parent_dir)
 
-        measurement = float(line[3])
+        steering = float(steering)
+        images, steerings = _process_img_and_steering(img_path_base + _get_image_path(center), steering)
+        return_images.extend(images)
+        return_steerings.extend(steerings)
 
-        if _left_turning(measurement):
-            # left turn for right image
-            source_path = line[2]
-            filename = source_path.split('/')[-1]
-            current_path = image_file_base + filename
-            image = cv2.imread(current_path)
-            image = _apply_random_brightness(image)
-            return_images.append(image)
-            return_images.append(cv2.flip(image, 1))
-            return_measurements.append(measurement-_CORRECTION_NUM)
-            return_measurements.append((measurement-_CORRECTION_NUM) * -1)
+        if _left_turning(steering):
+            images, steerings = _process_img_and_steering(img_path_base + _get_image_path(right), steering-_CORRECTION_NUM)
+            return_images.extend(images)
+            return_steerings.extend(steerings)
 
+        if _right_turning(steering):
+            images, steerings = _process_img_and_steering(img_path_base + _get_image_path(left), steering + _CORRECTION_NUM)
+            return_images.extend(images)
+            return_steerings.extend(steerings)
 
-        if _right_turning(measurement):
-            # right
-            source_path = line[1]
-            filename = source_path.split('/')[-1]
-            current_path = image_file_base + filename
-            image = cv2.imread(current_path)
-            image = _apply_random_brightness(image)
-            return_images.append(image)
-            return_images.append(cv2.flip(image, 1))
-
-            return_measurements.append(measurement+_CORRECTION_NUM)
-            return_measurements.append((measurement+_CORRECTION_NUM) * -1)
-
-    return return_images, return_measurements
+    return return_images, return_steerings
 
 
-def _get_image_path(path):
-    return path.split('/')[-1]
+def _process_img_and_steering(image_full_path, steering):
+    to_return_images, to_return_steerings = [], []
+    image = cv2.imread(image_full_path)
+    assert image is not None
+    image = _apply_random_brightness(image)
 
+    # add the image and steering as it is
+    to_return_images.append(image)
+    to_return_steerings.append(steering)
 
-def _left_turning(angle):
-    return angle < (-1 * _SHARP_ANGEL_THESHOLD)
-
-
-def _right_turning(angle):
-    return angle > 1 * _SHARP_ANGEL_THESHOLD
-
-
-def _get_csv_lines(parent_dir):
-    lines = []
-    # formatting of the CSV:
-    # ['center', 'left', 'right', 'steering', 'throttle', 'brake', 'speed']
-
-    csv_file = _CSV_FILE_BASE.format(parent_dir)
-    image_file_base = _IMAGE_FILE_BASE.format(parent_dir)
-    with open(csv_file) as csvfile:
-        reader = csv.reader(csvfile)
-        for line in reader:
-            lines.append(line)
-
-    lines = iter(lines)
-    _ = next(lines)
-    return lines
+    # as the flipped version of image and steering
+    to_return_images.append(cv2.flip(image, 1))
+    to_return_steerings.append(steering * -1.0)
+    return to_return_images, to_return_steerings
 
 
 def _apply_random_brightness(img):
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     rand = random.uniform(0.3, 1.0)
     hsv[:,:,2] = rand*hsv[:,:,2]
-    new_img = cv2.cvtColor(hsv, cv2.COLOR_HSV2RGB)
-    return new_img
+    return cv2.cvtColor(hsv, cv2.COLOR_HSV2RGB)
 
 
 if __name__ == '__main__':
