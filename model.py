9ced55ea13442753d43fc40f409ada6f48a2be1f
bingbinglai <imbingbinglai@gmail.com>
2018-08-01 01:43:10 -0400

    refactor

--- model.py
+++ model.py
@@ -14,140 +14,43 @@ import random
 
 _CORRECTION_NUM = 0.02
 
-# ['center', 'left', 'right', 'steering', 'throttle', 'brake', 'speed']
 
-def helper(parent_dir):
-    lines = []
-    csv_file = './{}/driving_log.csv'.format(parent_dir)
-    image_file_base = './{}/IMG/'.format(parent_dir)
+_DROPOUT_RATE = 0.2
 
-    with open(csv_file) as csvfile:
-        reader = csv.reader(csvfile)
-        for line in reader:
-            lines.append(line)
+_SHARP_ANGEL_THESHOLD = 0.15
 
 
-    if parent_dir in [
-        'data',
-        # 'more-curves6',
-    ]:
-        lines = random.sample(lines, int(len(lines) * 0.4))
+'where the driving log are stored'
+_CSV_FILE_BASE = './all_training_data/useful/{}/driving_log.csv'
 
-    return_images = []
-    return_measurements = []
-
-    lines = iter(lines)
-    # remove headers
-    _ = next(lines)
-
-    for line in lines:
-        # center
-        source_path = line[0]
-        filename = source_path.split('/')[-1]
-        current_path = image_file_base + filename
-        image = cv2.imread(current_path)
-
-        if image is None:
-            continue
-
-        image = brightness(image)
-        return_images.append(image)
-        return_images.append(cv2.flip(image, 1))
-        measurement = float(line[3])
-        return_measurements.append(measurement)
-        return_measurements.append(measurement * -1.0)
-
-
-        angle = float(line[3])
-
-        if angle < -0.15:
-
-            # left turn for right image
-            source_path = line[2]
-            filename = source_path.split('/')[-1]
-            current_path = image_file_base + filename
-            image = cv2.imread(current_path)
-            image = brightness(image)
-            return_images.append(image)
-            return_images.append(cv2.flip(image, 1))
-
-            measurement = float(line[3])
-            return_measurements.append(measurement-_CORRECTION_NUM)
-            return_measurements.append((measurement-_CORRECTION_NUM) * -1)
-
-
-        if angle > 0.15:
-            # right
-            source_path = line[1]
-            filename = source_path.split('/')[-1]
-            current_path = image_file_base + filename
-            image = cv2.imread(current_path)
-            image = brightness(image)
-            return_images.append(image)
-            return_images.append(cv2.flip(image, 1))
-
-            measurement = float(line[3])
-            return_measurements.append(measurement+_CORRECTION_NUM)
-            return_measurements.append((measurement+_CORRECTION_NUM) * -1)
-
-
-    return return_images, return_measurements
-
-
-
-#random brightness of imgs
-def brightness(img):
-    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    rand = random.uniform(0.3, 1.0)
-    hsv[:,:,2] = rand*hsv[:,:,2]
-    new_img = cv2.cvtColor(hsv, cv2.COLOR_HSV2RGB)
-    return new_img
-
+"path to the corresponding image files that's recorded in the csvs"
+_IMAGE_FILE_BASE = './all_training_data/useful/{}/IMG/'
 
 
 def train():
     parent_dirs = [
-        # 'data',
-
+        # dataaa is the trainging data! train by ourlsef
+        'dataaa',
         # trying recovery_data
         # seems helpful! -- helps stablized
         '1_recovery_data',
-
-
+        # driving in the opposite direction
         'reverse-data',
-
-        # NOTE: trying not using more-curves
-        # it just bump into the bridge but it succesfully avoiding drifting into water
-        # using just more-curves2 and more-curves3
-
-        # 'more-curves',
+        # training around the curves
         'more-curves2-good-data',
         'more-curves3-good-data',
-
         # use cuurves 4!
         'more-curves-4',
-
-        # not so useful!
-        # 'more-curves-6',
-
         # use curves-new-6!
         'more-curves-new-6',
         # curves-7 is the opposite direction of curve-new-6,so...
         'more-curves-7',
-
-        # dataaa is the trainging data! train by ourlsef
-        'dataaa',
-
-        # 'more-curves-8',
-        # does not need curves5
-        # 'more-curves5',
-
     ]
 
     all_images = []
     all_measurements = []
     for the_dir in parent_dirs:
-        _images, _measurements = helper(the_dir)
+        _images, _measurements = _get_images_and_measurements(the_dir)
         print('dir: {}, images: {}'.format(the_dir, len(_images)))
         all_images.extend(_images)
         all_measurements.extend(_measurements)
@@ -155,15 +58,9 @@ def train():
 
     print('total images', len(all_images))
     print('total measurements', len(all_measurements))
-
-
     X_train = np.array(all_images)
     y_train = np.array(all_measurements)
 
-    print(X_train.shape)
-    print(y_train.shape)
-
-    _DROPOUT_RATE = 0.2
 
     model = Sequential()
     model.add(Lambda(lambda x: x / 255.0 -0.5, input_shape=(160,320,3)))
@@ -186,4 +83,100 @@ def train():
     model.save('model.h5')
     print('Done')
 
-train()
+
+def _get_images_and_measurements(parent_dir):
+    '''
+    given a parent directory, parse all the images and the angle
+    '''
+    return_images = []
+    return_measurements = []
+
+    for line in _get_csv_lines(parent_dir):
+
+        # center image
+        image = cv2.imread(
+            _IMAGE_FILE_BASE.format(_get_image_path(line[0]))
+        )
+
+        if image is None:
+            continue
+        image = _apply_random_brightness(image)
+
+        # add the image as it is and also when its flipped version
+        return_images.append(image)
+        return_images.append(cv2.flip(image, 1))
+        measurement = float(line[3])
+        return_measurements.append(measurement)
+        return_measurements.append(measurement * -1.0)
+
+
+        measurement = float(line[3])
+
+        if _sharp_left_turn(measurement):
+            # left turn for right image
+            source_path = line[2]
+            filename = source_path.split('/')[-1]
+            current_path = image_file_base + filename
+            image = cv2.imread(current_path)
+            image = _apply_random_brightness(image)
+            return_images.append(image)
+            return_images.append(cv2.flip(image, 1))
+            return_measurements.append(measurement-_CORRECTION_NUM)
+            return_measurements.append((measurement-_CORRECTION_NUM) * -1)
+
+
+        if _sharp_right_turn(measurement):
+            # right
+            source_path = line[1]
+            filename = source_path.split('/')[-1]
+            current_path = image_file_base + filename
+            image = cv2.imread(current_path)
+            image = _apply_random_brightness(image)
+            return_images.append(image)
+            return_images.append(cv2.flip(image, 1))
+
+            return_measurements.append(measurement+_CORRECTION_NUM)
+            return_measurements.append((measurement+_CORRECTION_NUM) * -1)
+
+    return return_images, return_measurements
+
+
+def _get_image_path(path):
+    return path.split('/')[-1]
+
+
+def _sharp_left_turn(angle):
+    return angle < (-1 * _SHARP_ANGEL_THESHOLD)
+
+
+def _sharp_right_turn(angle):
+    return angle > 1 * _SHARP_ANGEL_THESHOLD
+
+
+def _get_csv_lines(lines):
+    lines = []
+    # formatting of the CSV:
+    # ['center', 'left', 'right', 'steering', 'throttle', 'brake', 'speed']
+
+    csv_file = _CSV_FILE_BASE.format(parent_dir)
+    image_file_base = _IMAGE_FILE_BASE.format(parent_dir)
+    with open(csv_file) as csvfile:
+        reader = csv.reader(csvfile)
+        for line in reader:
+            lines.append(line)
+
+    lines = iter(lines)
+    _ = next(lines)
+    return lines
+
+
+def _apply_random_brightness(img):
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    rand = random.uniform(0.3, 1.0)
+    hsv[:,:,2] = rand*hsv[:,:,2]
+    new_img = cv2.cvtColor(hsv, cv2.COLOR_HSV2RGB)
+    return new_img
+
+
+if __name__ == '__main__':
+    exit(train())
